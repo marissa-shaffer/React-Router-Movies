012cd4d1131b0bbbaeeace485da9d7fae3502327
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-03-15 20:12:27 -0500

    Added Link to react router dom import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
