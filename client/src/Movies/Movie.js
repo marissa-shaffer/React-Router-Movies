7a4b9ce534a01bfe076aaecd8d2d0c3dd2ec2eea
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-03-15 19:44:56 -0500

    Changed line 8 to grab id from url. added array.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -19,6 +19,10 @@ const Movie = (props) => {
         });
 
   },[]);
+  
+  useEffect(() => {
+
+  }, [movie]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
